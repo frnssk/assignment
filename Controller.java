6a98388c518cbc78dcb294d13aa077611746633e
frnssk <41634799+frnssk@users.noreply.github.com>
2019-05-07 14:07:12 +0200

    Update Controller.java

--- Controller.java
+++ Controller.java
@@ -1,7 +1,7 @@
 package assignment3;
 
 public class Controller {
-	private Buffer buffer = new Buffer();
+	private Buffer2 buffer = new Buffer2();
 	private GUISemaphore gui = new GUISemaphore();
 	private Consumer ica;
 	private Consumer coop;
@@ -11,17 +11,17 @@ public class Controller {
 	private Producer axFood;
 	private int storageUsage = 0;
 
-	private String[] daryItems = { "Milk", "Ice cream", "Yoghurt", "Cottage cheese", "Cheese",
+	private String[] arlaItemNames = { "Milk", "Ice cream", "Yoghurt", "Cottage cheese", "Cheese",
 			"Butter", "Cream", "Cream cheese", "Sour cream", "Skyr",
 			"Aged chees", "Kefir", "Goat milk", "Goat cheese", "Frozen yoghurt",
 			"Eggnod", "Curd", "Condensed milk", "Chocolate milk", "ECO Butter"};
 
-	private String[] meetItems = {"Bacon", "Meetbals", "Smoked ham", "Cooked ham", "Christmas ham", 
+	private String[] scanItemNames = {"Bacon", "Meatballs", "Smoked ham", "Cooked ham", "Christmas ham", 
 			"Smoked bacon", "Tenderloin", "Beef", "BBQ Bacon", "Spare ribs",
 			"Ribs", "BBQ Steak", "Steak", "Pork", "Smoked pork",
 			"liverwurst", "Salami", "Pepper salami", "Hot dogs", "Chorizo"};
 
-	private String[] otherItems = {"Olive oil", "Pepsi", "Zingo", "7up", "Coca cola", 
+	private String[] axFoodItemNames = {"Olive oil", "Pepsi", "Zingo", "7up", "Coca cola", 
 			"Fanta", "Sprite", "Chips", "Tomatos", "Lemons", 
 			"Rice", "Spagetti", "Onions", "Corn Flakes", "Bread",
 			"Soy sauce", "Blueberrys", "Strawberrys", "Cookies", "Canned foods"};
@@ -44,44 +44,54 @@ public class Controller {
 		cityGross.start();
 
 		scan = new Producer("Scan", this, buffer);
-		scan.initFoodItems(meetItems);
+		scan.initFoodItems(scanItemNames);
 		scan.start();
 
 		arla = new Producer("Arla", this, buffer);
-		arla.initFoodItems(daryItems);
+		arla.initFoodItems(arlaItemNames);
 		arla.start();
 
-		axFood = new Producer("Axfood", this, buffer);
-		axFood.initFoodItems(otherItems);
+		axFood = new Producer("AxFood", this, buffer);
+		axFood.initFoodItems(axFoodItemNames);
 		axFood.start();
 	}
 
 	public void startScan() {
 		scan.startProducing(true);
+		gui.setStatusProducer("S", "Producing");
 	}
 
 	public void stopScan() {
 		scan.startProducing(false);
+		gui.setStatusProducer("S", "Stopped");
 	}
 
 	public void startArla() {
 		arla.startProducing(true);
+		gui.setStatusProducer("A", "Producing");
 	}
 
 	public void stopArla() {
 		arla.startProducing(false);
+		gui.setStatusProducer("A", "Stopped");
 	}
 
 	public void startAxFood() {
 		axFood.startProducing(true);
+		gui.setStatusProducer("X", "Producing");
 	}
 
 	public void stopAxFood() {
 		axFood.startProducing(false);
+		gui.setStatusProducer("X", "Stopped");
 	}
 	
 	public void startIca() {
 		ica.startConsuming(true);
+	}
+	
+	public boolean getIcaCont() {
+		return gui.getIcaCont();
 	}
 	
 	public void stopIca() {
@@ -92,12 +102,20 @@ public class Controller {
 		coop.startConsuming(true);
 	}
 	
+	public boolean getCoopCont() {
+		return gui.getCoopCont();
+	}
+	
 	public void stopCoop() {
 		coop.startConsuming(false);
 	}
 	
 	public void startCityGross() {
 		cityGross.startConsuming(true);
+	}
+	
+	public boolean getCGCont() {
+		return gui.getCGCont();
 	}
 	
 	public void stopCityGross() {
@@ -109,28 +127,32 @@ public class Controller {
 		gui.updateStatusBar(storageUsage);
 	}
 	
-	public void updateIca(int items, int weight, int volume) {
-		gui.updateIca(items, weight, volume);
+	public void updateIca(int items, int weight, int volume, String cargo) {
+		gui.updateIca(items, weight, volume, cargo);
 	}
 	
 	public void clearIca() {
 		gui.clearIca();
 	}
 	
-	public void updateCoop(int items, int weight, int volume) {
-		gui.updateCoop(items, weight, volume);
+	public void updateCoop(int items, int weight, int volume, String cargo) {
+		gui.updateCoop(items, weight, volume, cargo);
 	}
 	
 	public void clearCoop() {
 		gui.clearCoop();
 	}
 	
-	public void updateCityGross(int items, int weight, int volume) {
-		gui.updateCityGross(items, weight, volume);
+	public void updateCityGross(int items, int weight, int volume, String cargo) {
+		gui.updateCityGross(items, weight, volume, cargo);
 	}
 	
 	public void clearCityGross() {
 		gui.clearCityGross();
+	}
+	
+	public void setStatusConsumer(String consumer, String status) {
+		gui.setStatusConsumer(consumer, status);
 	}
 
 }
